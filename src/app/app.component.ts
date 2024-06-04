c7f1a9c85c9db6f05ae7a5f8b30eda8f7f3f249f
Gilles Ferrand <gilles.ferrand@nurun.com>
2024-06-03 20:18:56 -0400

    Just a quick demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import { z } from "zod";
+
+
+// With Zod
+const userSchema = z.object({
+  name: z.string(),
+}).readonly();
+const userListSchema = z.array(userSchema).readonly();
+type UserList = z.infer<typeof userListSchema>;
+
+// Without Zod
+type UserListWithoutZod = readonly { readonly name: string }[];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <h1>Here for the infer weird bug</h1>
+    @for(user of userList; track user) {
+      <p>{{ user.name }}</p>
+    }
+    @for(user of userListWithoutZod; track user) {
+      <p>{{ user.name }}</p>
+    }
+  `,
 })
-export class AppComponent {
-  title = 'infer-bug-intellij';
+export class AppComponent implements OnInit {
+  public userList: UserList = [{ name: 'John' }, { name: 'Doe' }];
+  public userListWithoutZod: UserListWithoutZod = [{ name: 'John' }, { name: 'Doe' }];
+
+  public ngOnInit(): void {
+    for (const user of this.userList) {
+      // Well inferred here
+      console.log(user.name);
+    }
+  }
 }
